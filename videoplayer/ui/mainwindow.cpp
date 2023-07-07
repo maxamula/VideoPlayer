29f3d2b5e21f729d4dd7eb031e4a1c33ca18022d
maxamula <78308778+maxamula@users.noreply.github.com>
2023-07-07 21:02:40 +0300

    Shutdown fix

--- videoplayer/ui/mainwindow.cpp
+++ videoplayer/ui/mainwindow.cpp
@@ -3,9 +3,6 @@
 #include <QFileDialog>
 #include <iostream>
 
-MainWindow::MediaEvent::MediaEvent(IMFMediaEvent* pMediaEvent, MediaEventType type)
-    : QEvent(EVENT_MEDIA), m_pMediaEvent(pMediaEvent), m_type(type)
-{ }
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,35 +12,12 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui.btnPlay, &QPushButton::pressed, this, &MainWindow::Play);
     connect(ui.btnPause, &QPushButton::pressed, this, &MainWindow::Pause);
     connect(ui.btnStop, &QPushButton::pressed, this, &MainWindow::Stop);
-
-    //m_pPlayer->SetInput(L"C:\\Users\\maxamula\\Desktop\\ee.mp4"); // TODO remove
 }
 
 MainWindow::~MainWindow()
 {}
 
-void MainWindow::PostMediaEvent(IMFMediaEvent* pMediaEvent, MediaEventType type)
-{
-    QApplication::postEvent(this, new MediaEvent(pMediaEvent, type));
-}
-
-void MainWindow::customEvent(QEvent* event)
-{
-    MainWindow::MediaEvent* pMediaEvent = dynamic_cast<MainWindow::MediaEvent*>(event);
-
-}
-
-void MainWindow::resizeEvent(QResizeEvent* event)
-{
-    QMainWindow::resizeEvent(event);
-}
-
 #pragma region SLOTS
-
-void MainWindow::OpenFile()
-{
-    
-}
 
 void MainWindow::Play()
 {
